ec06148762ef344f99852dea655aea600093f64e
SIVABALAN V <sivabalanv2005@gmail.com>
2026-02-09 09:58:44 +0530

    Add Hollow Pyramid pattern printing program
    Implemented a program to print a hollow pyramid pattern based on user input for character and height.

--- Pattern/Hollow_Pyramid.java
+++ Pattern/Hollow_Pyramid.java
@@ -1,1 +1,26 @@
+package Pattern;
+import java.util.Scanner;
+public class Hollow_Pyramid {
 
+	public static void main(String[] args) {
+		// TODO a hollow pyramid
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number of charecters : ");
+		int n = in.nextInt();
+		System.out.println("Enter the Charecter to be printed " );
+		String c= in.next();
+		for (int i=1;i<=n;i++) {
+			for(int j=i;j<n;j++) {
+				System.out.print(" ");	
+			}
+			for(int k=1;k<=2*i-1;k++) {
+				if (k==1||k==2*i-1||i==n)
+					System.out.print(c);
+				else
+					System.out.print(" ");
+			}
+			System.out.println("");
+		}
+		in.close();
+	}
+}
